57cbff4c613dd1f58f1606238be9628d846ff32b
Chin Arinze <c.s.arinze@gmail.com>
2022-01-26 02:28:07 +0000

    Options
    Argparse was imported to make adding options to this script much easier. Now a user can select to decrypt or encrypt their message (a moot point considering how rot13 works but good practice). The script needs to be cleaned up still and a default option will need to be introduced in the event the user does not select a flag. I may also opt to add inline support to supply the message to encrypt rather than sending the user through the script.

--- substitution_ciphers/rot13.py
+++ substitution_ciphers/rot13.py
@@ -1,40 +1,38 @@
 #!/usr/bin/python 
 import string as STRING
+import argparse
+
+parser = argparse.ArgumentParser()
+group = parser.add_mutually_exclusive_group()
+group.add_argument("-d", "--decrypt", help="Use this flag to decrypt the message you provide", action="store_true")
+group.add_argument("-e", "--encrypt", help="Use this flag to encrypt the message you provide", action="store_true")
+args = parser.parse_args()
 
 
-def decrypt(string):
+def encrypt(string, key):
   # takes in a string and rotates each character by 13 places 
   arr = []
   for el in string:
     print(el)
     if el in STRING.ascii_lowercase:
-      char = chr((ord(el) - 96 - 13) % 26 + 96)
+      char = chr((ord(el) - 96 + key) % 26 + 96)
       arr.append(char)
     elif el in STRING.ascii_uppercase:
-      char = chr((ord(el) - 64 - 13) % 26 + 64)
+      char = chr((ord(el) - 64 + key) % 26 + 64)
       arr.append(char)
     else:
       arr.append(el)
   return ''.join(arr)
 
-def encrypt(string):
-  # takes in a string and rotates each character by 13 places 
-  arr = []
-  for el in string:
-    print(el)
-    if el in STRING.ascii_lowercase:
-      char = chr((ord(el) - 96 + 13) % 26 + 96)
-      arr.append(char)
-    elif el in STRING.ascii_uppercase:
-      char = chr((ord(el) - 64 + 13) % 26 + 64)
-      arr.append(char)
-    else:
-      arr.append(el)
-  return ''.join(arr)
-
-def run():
-  string = input("Please enter the message you would like to encrypt: ")
-  encrypted_value = decrypt(string)
+def run(key):
+  string = input("Please enter the message: ")
+  encrypted_value = encrypt(string, key)
   print(encrypted_value)
 
-run()
+#run()
+if args.decrypt:
+  print("decrypting...")
+  run(-13)
+elif args.encrypt:
+  print("encrypting...")
+  run(13)
