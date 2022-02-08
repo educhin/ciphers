26c5adade4872db490fc615758b4ca459140e876
Chin Arinze <c.s.arinze@gmail.com>
2022-02-08 14:22:40 +0000

    Update command line protocol
    The latest updates to the program make the run method obsolete. Currently the program works in line from the terminal when passing a string and a flag. The next additions will be to allow the user to pass a file to the program in addition to the option to pass a string.

--- substitution_ciphers/rot13.py
+++ substitution_ciphers/rot13.py
@@ -4,8 +4,8 @@ import argparse
 
 parser = argparse.ArgumentParser()
 group = parser.add_mutually_exclusive_group()
-group.add_argument("-d", "--decrypt", help="Use this flag to decrypt the message you provide", action="store_true")
-group.add_argument("-e", "--encrypt", help="Use this flag to encrypt the message you provide", action="store_true")
+group.add_argument("-d", "--decrypt", type=str, nargs=1, default=None, help="Use this flag to decrypt the message you provide")
+group.add_argument("-e", "--encrypt", type=str, nargs=1, default=None, help="Use this flag to encrypt the message you provide" )
 args = parser.parse_args()
 
 
@@ -13,7 +13,7 @@ def encrypt(string, key):
   # takes in a string and rotates each character by 13 places 
   arr = []
   for el in string:
-    print(el)
+    #print(el)
     if el in STRING.ascii_lowercase:
       char = chr((ord(el) - 96 + key) % 26 + 96)
       arr.append(char)
@@ -30,9 +30,11 @@ def run(key):
   print(encrypted_value)
 
 #run()
-if args.decrypt:
+if args.decrypt != None:
   print("decrypting...")
-  run(-13)
-elif args.encrypt:
+  encrypted_value = encrypt(args.decrypt[0], -13)
+  print(encrypted_value)
+elif args.encrypt != None:
   print("encrypting...")
-  run(13)
+  encrypted_value = encrypt(args.encrypt[0], 13)
+  print(encrypted_value)
